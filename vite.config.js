28a236bc133f688359bff33df7655d6da3738639
lucas.mendes <lucas.batista@consensotec.com.br>
2023-09-25 23:00:11 -0300

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/cdn/',
   build: {
     outDir: 'dist/cdn',
     emptyOutDir: true
