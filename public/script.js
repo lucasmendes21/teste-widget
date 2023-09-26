1db228b72d0a19f3d8f50e8c00c561ec10d7a025
lucas.mendes <lucas.batista@consensotec.com.br>
2023-09-25 23:01:54 -0300

    update iframe url

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 (function() {
   const iframe = document.createElement('iframe');
-  iframe.src = 'https://teste-widget.vercel.app';
+  iframe.src = 'https://teste-widget.vercel.app/cdn';
   iframe.width = '100%';
   iframe.height = '400';
 
