1b6c6719099d19840d90a3e04881fef744839148
lucas.mendes <lucas.batista@consensotec.com.br>
2023-10-12 02:27:22 -0300

    update widget vercel

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
   const iframe = document.createElement('iframe');
   iframe.src = 'https://teste-widget.vercel.app/cdn';
   iframe.width = '100%';
-  iframe.height = '400';
+  iframe.height = '600';
 
   document.getElementById('render-chart').appendChild(iframe)
 })();
